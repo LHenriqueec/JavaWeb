62fa1b4fe41664ec6612c9d9da89cfd8dc401de2
Luiz Henrique <luizsnip@gmail.com>
2016-07-15 23:18:15 -0300

    Utilizando EL e JSTL

---
+++ Exercicio2/EL&JSTL/exercicio2/servlet/ListaServlet.java
@@ -0,0 +1,32 @@
+package exercicio2.servlet;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Map<String, Object> map = new TreeMap<>();
+		Enumeration<String> enums = request.getServletContext().getAttributeNames();
+		
+		while (enums.hasMoreElements()) {
+			String chave = enums.nextElement();
+			Object valor = request.getServletContext().getAttribute(chave);
+			map.put(chave, valor);
+		}
+
+		request.setAttribute("parametros", map.entrySet());
+		
+		request.getRequestDispatcher("/Exercicio2/lista.jsp").forward(request, response);
+	
+	}
+}
---
+++ WebContent/Exercicio2/lista.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Lista Exercicio 2 - EL e JSTL</title>
+</head>
+<body>
+
+	<table border="1" width="100%">
+		<thead>
+			<tr>
+				<td align="center">Nome</td>
+				<td align="center">Valor</td>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach var="p" items="${parametros}">
+				<tr>
+					<td align="center">${p.getKey()}</td>
+					<td align="center">${p.getValue()}</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+
+</body>
+</html>
